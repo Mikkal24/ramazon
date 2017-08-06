808e74bf764111599b1704e45d3ee86ad6588313
Alex <alex.d.escobedo@gmail.com>
2017-08-05 20:46:42 -0700

    Update Main.js

--- app/components/Main.js
+++ app/components/Main.js
@@ -6,15 +6,17 @@ var Main = React.createClass({
 	render: function(){
 		return(
 			<div className="page-flexbox-wrapper">
-				<header>
-					<Navbar />
-				</header>
-				<main className="centerThis">
-					{this.props.children}
-				</main>
-				<footer className="amazonBlue page-footer">
-					<Footer />
-				</footer>
+				<form action="/your-server-side-code" method="POST">
+  <script
+    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+    data-key="pk_test_5d9CqeRNmhQBq86Qj1slB5sj"
+    data-amount="999"
+    data-name="ramazongen.herokuapp"
+    data-description="Widget"
+    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+    data-locale="auto">
+  </script>
+</form>
 			</div>
 			)
 	}
