23aff26b4d641c68ed4e52bad21441646cd2a1bb
Michael Sorensen <voltx180@gmail.com>
2018-06-02 13:10:05 -0700

    jQuery is officially no more

--- app/components/centerBox/buyBoxChildren/BillingForm.js
+++ app/components/centerBox/buyBoxChildren/BillingForm.js
@@ -13,14 +13,6 @@ class BillingForm extends Component {
       range: this.props.queryInfo.amount,
       shippingAddress: this.props.queryInfo.shippingInfo
     };
-
-    // query.getRandomCart(queryObj).then(response => {
-    //   var cartLink = $("<a>")
-    //     .addClass("btn orange")
-    //     .attr({ href: response.data, target: "_blank" })
-    //     .text("But you can see what you would have got!");
-    //   $("#target").append(cartLink);
-    // });
   };
 
   sendData = () => {
