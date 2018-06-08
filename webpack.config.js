4f68ffd9dd464a1a6429c91c31905a4d35486eb5
Michael Sorensen <voltx180@gmail.com>
2018-06-07 22:30:47 -0700

    updated webpack version to 4.1

--- webpack.config.js
+++ webpack.config.js
@@ -9,21 +9,32 @@ module.exports = {
 
   // This section desribes the transformations we will perform
   module: {
-    loaders: [
+    rules: [
       {
-        // Only working with files that in in a .js or .jsx extension
-        test: /\.jsx?$/,
-        // Webpack will only process files in our app folder. This avoids processing
-        // node modules and server files unnecessarily
-        include: /app/,
-        loader: "babel",
-        query: {
-          // These are the specific transformations we'll be using.
-          presets: ["react", "es2015"],
-          plugins: ["transform-class-properties"]
-        }
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
       }
     ]
+
+    // loaders: [
+    //   {
+    //     // Only working with files that in in a .js or .jsx extension
+    //     test: /\.jsx?$/,
+    //     // Webpack will only process files in our app folder. This avoids processing
+    //     // node modules and server files unnecessarily
+    //     include: /app/,
+    //     loader: "babel",
+    //     query: {
+    //       // These are the specific transformations we'll be using.
+    //       presets: ["react", "es2015"],
+    //       plugins: ["transform-class-properties"]
+    //     }
+    //   }
+    // ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
   },
   // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
   // Without this the console says all errors are coming from just coming from bundle.js
