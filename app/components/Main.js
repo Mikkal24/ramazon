eea2724e56b192d2b40fe18dfa7e204d3eb6a648
Michael Sorensen <voltx180@gmail.com>
2018-06-16 20:31:26 -0700

    removed background stuffs

--- app/components/Main.js
+++ app/components/Main.js
@@ -6,14 +6,6 @@ import { Navbar, Footer } from "./mainChildren/index";
 import styles from "./styles.less";
 
 class Main extends Component {
-  setGoldBackground = () => {
-    this.setState({ backgroundClass: styles.gold });
-  };
-
-  setGrayBackground = () => {
-    this.setState({ backgroundClass: styles.rhino });
-  };
-
   render = () => {
     return (
       <div className={styles.mainContainer}>
